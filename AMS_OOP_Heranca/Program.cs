c98ebb5e64064b8fbd11ddca7b7bd333b1ea4899
2joaomiguel6 <102696102+2joaomiguel6@users.noreply.github.com>
2023-07-27 09:32:12 -0300

    Update Program.cs
    alteração

--- AMS_OOP_Heranca/Program.cs
+++ AMS_OOP_Heranca/Program.cs
@@ -6,6 +6,6 @@ Car c1 = new Car("BO2A52","Fusca", 5, 24);
 Console.WriteLine("Plane: \n" + "Airplane id: " + a1.getId()+ "\nAirplane Description: "+ a1.getDes() + "\nAirplane Capacity Seat: "+ a1.PgetCap()+ "\nAirplane km: "+ a1.Pgetkm());
 Console.WriteLine(a1.Pilot());
 Console.WriteLine("=============================");
+Console.WriteLine("=============================")
 Console.WriteLine("Car: \n"+"Car id: " + c1.getId()+ "\nCar Description: "+ c1.getDes() + "\nCar Capacity Seat: "+ c1.CgetCap()+"\nCar km: "+ c1.CgetKm());
 Console.WriteLine(c1.drive());
-Console.WriteLine("=============================");
